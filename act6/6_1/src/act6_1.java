ea9059f621625190de0abbc3bd33771bdd0d4fa4
Katlhyn Montoya Valverde <emonval715@g.educaand.es>
2025-10-02 15:41:49 +0200

    Add files via upload

---
+++ act6/6_1/src/act6_1.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class act6_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Declaración de variables
+        int n;
+
+        //Asociación de "n" al mes
+        System.out.println("Introduzca el número de un mes:");
+        n = sc.nextInt();
+
+        switch(n) {
+            case 1:
+                System.out.println("Enero");
+                break;
+            case 2:
+                System.out.println("Febrero");
+                break;
+            case 3:
+                System.out.println("Marzo");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Mayo");
+                break;
+            case 6:
+                System.out.println("Junio");
+                break;
+            case 7:
+                System.out.println("Julio");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Septiembre");
+                break;
+            case 10:
+                System.out.println("Octubre");
+                break;
+            case 11:
+                System.out.println("Noviembre");
+                break;
+            case 12:
+                System.out.println("Diciembre");
+                break;
+            default:
+                System.out.println("Número invalido");
+        }
+    }
+}
