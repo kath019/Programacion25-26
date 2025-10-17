97cb6a1298392ca4aa417b28d8ffc2e03a93324a
Katlhyn Montoya Valverde <emonval715@g.educaand.es>
2025-10-17 10:54:29 +0200

    Delete act2/2_1/act2_1 directory

--- act2/2_1/act2_1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int n=4;
-        int m=6;
-        System.out.println("Producto de 4 y 6");
-        System.out.println(n*m);
-    }
-}
